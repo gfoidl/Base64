9fe5a09482935047b10a0cc1ce33fb66a12fcccd
Günther Foidl <gue@korporal.at>
2018-11-21 19:25:51 +0100

    Encode with buffer-chains

--- source/gfoidl.Base64/IBase64Encoder.cs
+++ source/gfoidl.Base64/IBase64Encoder.cs
@@ -9,8 +9,8 @@ namespace gfoidl.Base64
         int GetDecodedLength(ReadOnlySpan<byte> encoded);
         int GetDecodedLength(ReadOnlySpan<char> encoded);
 
-        OperationStatus Encode(ReadOnlySpan<byte> data, Span<byte> encoded, out int consumed, out int written);
-        OperationStatus Encode(ReadOnlySpan<byte> data, Span<char> encoded, out int consumed, out int written);
+        OperationStatus Encode(ReadOnlySpan<byte> data, Span<byte> encoded, out int consumed, out int written, bool isFinalBlock = true);
+        OperationStatus Encode(ReadOnlySpan<byte> data, Span<char> encoded, out int consumed, out int written, bool isFinalBlock = true);
 
         OperationStatus Decode(ReadOnlySpan<byte> encoded, Span<byte> data, out int consumed, out int written);
         OperationStatus Decode(ReadOnlySpan<char> encoded, Span<byte> data, out int consumed, out int written);
