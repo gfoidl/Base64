c0aa49a59f30d8b3b86ee7a53568778db2f7916a
Günther Foidl <gue@korporal.at>
2018-11-25 18:23:54 +0100

    Added Base64 for easy usage (as singleton)

---
+++ source/gfoidl.Base64/Base64.cs
@@ -0,0 +1,11 @@
+﻿namespace gfoidl.Base64
+{
+    public static class Base64
+    {
+        private static readonly IBase64Encoder _default = new Base64Encoder();
+        private static readonly IBase64Encoder _url     = new Base64UrlEncoder();
+        //---------------------------------------------------------------------
+        public static IBase64Encoder Default => _default;
+        public static IBase64Encoder Url     => _url;
+    }
+}
