1d516a0e6bcb36ea61bea7252364c4d491ecd938
Günther Foidl <gue@korporal.at>
2018-11-25 19:10:03 +0100

    Added xml comments to interface and Base64 only
    It doesn't clutter the rest of the code.

--- source/gfoidl.Base64/Base64.cs
+++ source/gfoidl.Base64/Base64.cs
@@ -1,11 +1,21 @@
 ﻿namespace gfoidl.Base64
 {
+    /// <summary>
+    ///  Base64 encoding / decoding.
+    /// </summary>
     public static class Base64
     {
         private static readonly IBase64Encoder _default = new Base64Encoder();
         private static readonly IBase64Encoder _url     = new Base64UrlEncoder();
         //---------------------------------------------------------------------
+        /// <summary>
+        /// The base64 encoder / decoder.
+        /// </summary>
         public static IBase64Encoder Default => _default;
-        public static IBase64Encoder Url     => _url;
+
+        /// <summary>
+        /// The base64Url encoder / decoder.
+        /// </summary>
+        public static IBase64Encoder Url => _url;
     }
 }
