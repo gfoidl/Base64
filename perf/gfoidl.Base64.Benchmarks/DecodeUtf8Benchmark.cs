78cceb2d0048b7a1ca333fdc07348bed461a87dc
Günther Foidl <gue@korporal.at>
2018-11-28 14:57:20 +0100

    Adjusted benchmark for validation / development purposes
    This commit will be reverted.

--- perf/gfoidl.Base64.Benchmarks/DecodeUtf8Benchmark.cs
+++ perf/gfoidl.Base64.Benchmarks/DecodeUtf8Benchmark.cs
@@ -5,14 +5,18 @@ using gfoidl.Base64.Internal;
 
 namespace gfoidl.Base64.Benchmarks
 {
-    [Config(typeof(HardwareIntrinsicsCustomConfig))]
+    [ShortRunJob]
     public class DecodeUtf8Benchmark
     {
+        private static readonly Base64Encoder  s_encoder  = new Base64Encoder();
+        private static readonly Base64Encoder1 s_encoder1 = new Base64Encoder1();
+        private static readonly Base64Encoder2 s_encoder2 = new Base64Encoder2();
+        //---------------------------------------------------------------------
         private byte[] _base64;
         private byte[] _decoded;
         //---------------------------------------------------------------------
-        [Params(5, 16, 1_000)]
-        public int DataLen { get; set; } = 16;
+        //[Params(5, 16, 1_000)]
+        public int DataLen { get; set; } = 1_000;
         //---------------------------------------------------------------------
         [GlobalSetup]
         public void GlobalSetup()
@@ -29,15 +33,21 @@ namespace gfoidl.Base64.Benchmarks
         }
         //---------------------------------------------------------------------
         [Benchmark(Baseline = true)]
-        public OperationStatus BuffersBase64()
+        public OperationStatus Base()
         {
-            return System.Buffers.Text.Base64.DecodeFromUtf8(_base64, _decoded, out int _, out int _);
+            return s_encoder2.Decode(_base64, _decoded, out int _, out int _);
         }
         //---------------------------------------------------------------------
         [Benchmark]
-        public OperationStatus gfoidlBase64()
+        public OperationStatus Dev()
         {
-            return Base64.Default.Decode(_base64, _decoded, out int _, out int _);
+            return s_encoder.Decode(_base64, _decoded, out int _, out int _);
+        }
+        //---------------------------------------------------------------------
+        [Benchmark]
+        public OperationStatus PureSSSE3()
+        {
+            return s_encoder1.Decode(_base64, _decoded, out int _, out int _);
         }
     }
 }
