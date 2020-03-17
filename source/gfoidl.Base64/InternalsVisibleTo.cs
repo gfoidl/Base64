56c45f12a9a392e5d944bbea08a5192beb2ef372
Günther Foidl <gue@korporal.at>
2020-03-17 14:17:45 +0100

    Added strong name signing (#117)
    * Added strong name signing
    
    * Test to verify strong name is correct

--- source/gfoidl.Base64/InternalsVisibleTo.cs
+++ source/gfoidl.Base64/InternalsVisibleTo.cs
@@ -1,5 +1,10 @@
 ﻿using System.Runtime.CompilerServices;
 
-[assembly: InternalsVisibleTo("gfoidl.Base64.Benchmarks")]
-[assembly: InternalsVisibleTo("gfoidl.Base64.FuzzTests")]
-[assembly: InternalsVisibleTo("gfoidl.Base64.Tests")]
+[assembly: InternalsVisibleTo("gfoidl.Base64.Benchmarks, Publickey=" + Keys.SignKey)]
+[assembly: InternalsVisibleTo("gfoidl.Base64.FuzzTests, Publickey="  + Keys.SignKey)]
+[assembly: InternalsVisibleTo("gfoidl.Base64.Tests, Publickey="      + Keys.SignKey)]
+
+internal static class Keys
+{
+    public const string SignKey = "00240000048000009400000006020000002400005253413100040000010001009102c074550e276c36b61e473783fb81bece760951fe0d55c21fe7d8296e174cb41fb4f57a91544f3d597ba044e0278fb2fbb5af1b6fa697e20ca1707f907bb687b73b6ef8ec578404ed49d2206d0f485d4073b88722d92e1f1018e2467800e760b1a8ee478aec551b2975e01ae25140debfae1680042cdd4c80950909a8e8fc";
+}
---
+++ tests/gfoidl.Base64.Tests/Assembly.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using NUnit.Framework;
+
+namespace gfoidl.Base64.Tests
+{
+    [TestFixture]
+    public class Assembly
+    {
+        [Test]
+        public void StrongName_is_correct()
+        {
+            Type type           = typeof(Base64);
+            string assemblyName = type.Assembly.FullName;
+
+            Match match = Regex.Match(assemblyName, @"^gfoidl\.Base64, Version=[^,]*, Culture=neutral, PublicKeyToken=(.*)$");
+
+            Assert.Multiple(() =>
+            {
+                Assert.IsTrue(match.Success);
+
+                string publicKeyToken = match.Groups[1].Value;
+                Assert.AreEqual("6a1c26790d4ba8ae", publicKeyToken);
+            });
+        }
+    }
+}
